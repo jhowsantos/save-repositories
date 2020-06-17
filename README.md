f70b92843c8d689ff5fc2c3c09e453f54364630a
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-06-17 15:58:31 -0300

    push web challenge

---
+++ README.md
@@ -0,0 +1,49 @@
+<div align="center">
+  <img alt="Rocketseat Logo" src="https://avatars1.githubusercontent.com/u/28929274?s=280&v=4" width="150" />
+  <img alt="Github Logo" src="https://image.flaticon.com/icons/png/512/25/25231.png" width="150" />
+</div>
+
+<h1 align="center">
+  Save repositories - Web
+</h1>
+
+
+
+## :pushpin: Tecnologias 
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [ReactJS](https://pt-br.reactjs.org/)
+- [Axios](https://github.com/axios/axios)
+
+## :computer: Projeto
+
+Aplicação para armazenar repositórios do seu portfólio, que permite a criação, listagem e remoção dos repositórios  
+
+## :point_down: Executando o projeto
+
+No seu terminal copie e cole ou digite o comando abaixo:
+
+```git
+git clone https://github.com/jhowsantos/save-repositories-web.git
+````
+
+Após realizar o clone acesse a pasta do projeto:
+
+```git
+cd save-repositories-web
+````
+
+Logo em seguida digite o comando __*yarn*__ para baixar e atualizar as dependências do projeto  
+
+Para executar o projeto digite em seu terminal o comando:
+
+```terminal
+yarn start
+````
+
+_Lembre-se que para executar esse projeto é necessário ter o backend rodando_  
+[__*Acesse o backend aqui*__](https://github.com/jhowsantos/save-repositories-backend.git)
+
+---
+Feito com coração, café e muito esforço :heart: :rocket:
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState , useEffect } from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  async function handleAddRepository() {
+
+    const response = await api.post('repositories', {
+      title: "Desafio ReactJS",
+      url: "https://github.com/jhowsantos",
+      techs: ["ReactJS", "NodeJS"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+
+    const response = await api.delete(`repositories/${id}`);
+    
+    if(response.status === 204){
+      const updateRepository = repositories.filter(repository => repository.id !== id);
+      setRepositories(updateRepository);
+    }
+  }
+
+  return (
+    <div>
+
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+        ))}
+      </ul>
+
+      <br/>
+      <br/>
+        <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
